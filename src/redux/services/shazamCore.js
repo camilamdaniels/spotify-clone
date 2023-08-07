028b6b767f5d3087eb9d3193e20227db85ef340e
Camila Daniels <camiladaniels@Camilas-MacBook-Pro.local>
2023-08-06 23:15:38 -0500

    Search component complete.

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -27,10 +27,10 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({ query: () => 'v1/charts/world'}),
-        // getSongsByGenre: builder.query({ query: () => ``}),
-        // getSongsByCountry: builder.query({ query: () => ``}),
-        // getSongsBySearch: builder.query({ query: () => ``}),
-        // getArtistDetails: builder.query({ query: () => ``}),
+        getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}`}),
+        getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}`}),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`}),
+        getArtistDetails: builder.query({ query: (artistId) => `v1/artists/details?artist_id=${artistId}`}),
         getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}`}),
         getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}`})
     })
@@ -38,10 +38,10 @@ export const shazamCoreApi = createApi({
 
 export const {
     useGetTopChartsQuery,
-    // useGetSongsByGenreQuery,
-    // useGetSongsByCountryQuery,
-    // useGetSongsBySearchQuery,
-    // useGetArtistDetailsQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
+    useGetArtistDetailsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery
 } = shazamCoreApi;
